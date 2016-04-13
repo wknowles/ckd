2f596746a2eb13abe0fc8707732892734d499189
William Knowles <will@hollyandwill.com>
2016-04-13 09:09:28 +0100

    add plugin thumbext and create 2x thumbnails

--- site/snippets/projects.php
+++ site/snippets/projects.php
@@ -2,24 +2,9 @@
   <ul>
     <?php foreach(page('projects')->grandChildren()->visible() as $project): ?>
       <li><a href="<?php echo $project->url() ?>" title="<?php echo $project->title()->html() ?>" >
-          <img src="<?php echo $project->images()->first()->crop(250, null, 75)->url() ?>" alt="<?php echo $project->title()->html() ?>" width="auto" height="auto"></a>
+          <?php echo ThumbExt($project->images()->first(), array('width' => 250, 'height' => 250, 'crop' => true, 'quality' => 70 , 'srcset' => '2x')) ?>
+          </a>
       </li>
     <?php endforeach ?>
   </ul>
 </div>
-
-<?php /* ?>
- <ul class="teaser cf">
-  <?php foreach(page('projects')->children()->visible() as $project): ?>
-  <li>
-    <h3><a href="<?php echo $project->url() ?>"><?php echo $project->title()->html() ?></a></h3>
-    <p><?php echo $project->text()->excerpt(80) ?> <a href="<?php echo $project->url() ?>">read&nbsp;more&nbsp;→</a></p>
-    <?php if($image = $project->images()->sortBy('sort', 'asc')->first()): ?>
-    <a href="<?php echo $project->url() ?>">
-      <img src="<?php echo $image->url() ?>" alt="<?php echo $project->title()->html() ?>" >
-    </a>
-    <?php endif ?>
-  </li>
-  <?php endforeach ?>
-</ul>
-<?php // */ ?>
