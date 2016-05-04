2ebf32de8e84d1d7d8b8e7c7876cfddead987746
will@CKD <onamatapier@gmail.com>
2016-05-04 12:11:21 +0100

    begin rearrange projects

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,7 +1,7 @@
 <?php snippet('header') ?>
      <!-- slider section / main content -->
     <div id="slider" class="slider">
-      <img data-src="<?php echo url('assets/images/home-slider/01.jpg') ?>" data-src-2x="<?php echo url('assets/images/home-slider/01@2.jpg') ?>" width="960" height="420">
+      <img src="<?php echo url('assets/images/home-slider/01.jpg') ?>" src-2x="<?php echo url('assets/images/home-slider/01@2.jpg') ?>" width="960" height="420">
       <img data-src="<?php echo url('assets/images/home-slider/02.jpg') ?>" data-src-2x="<?php echo url('assets/images/home-slider/02@2.jpg') ?>" width="960" height="420">
       <img data-src="<?php echo url('assets/images/home-slider/03.jpg') ?>" data-src-2x="<?php echo url('assets/images/home-slider/03@2.jpg') ?>" width="960" height="420">
     </div>
